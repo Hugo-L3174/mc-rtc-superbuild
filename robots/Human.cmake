2291212cf8d08fccf569d331ca211fe1529f4919
Hugo L <lefevrehugo@ymail.com>
2025-03-03 15:13:07 +0100

    Config for my mc_rtc fork + my human model

--- robots/Human.cmake
+++ robots/Human.cmake
@@ -4,16 +4,20 @@ if(NOT WITH_HUMAN)
   return()
 endif()
 
-AddCatkinProject(
-  human_description
-  GITHUB jrl-umi3218/human_description
+# AddCatkinProject(
+#   human_description
+#   GITHUB jrl-umi3218/human_description
+AddCatkinProject(human_description
+  GITE hlefevre/human_description
   GIT_TAG origin/master
   WORKSPACE data_ws
 )
 
-AddProject(
-  mc_human
-  GITHUB jrl-umi3218/mc_human
+# AddProject(
+#   mc_human
+#   GITHUB jrl-umi3218/mc_human
+AddProject(mc_human
+  GITE hlefevre/mc_human
   GIT_TAG origin/master
   DEPENDS human_description mc_rtc
 )
